46bac8ad5eb341298114dfacd99820bc64a13c81
tatianagritsenko <99836203+tatianagritsenko@users.noreply.github.com>
2023-12-12 14:11:11 +0300

    Update game.cpp
    Draw game elements

--- game.cpp
+++ game.cpp
@@ -7,10 +7,15 @@ int main() {
 	const int screen_height = 800;
 	InitWindow(screen_width, screen_height, "My Pong Game!");
 	SetTargetFPS(60);
-	
+
 	while (WindowShouldClose() == false) {
 		BeginDrawing();
-
+		
+		DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+		DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+		DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
+		DrawRectangle(screen_width-35, screen_height / 2 - 60, 25, 120, WHITE);
+		
 		EndDrawing();
 	}
 
